42eadc3d71116f52436a34cf5644acfb7c9eb69e
cevik <suleymancevik64@gmail.com>
2022-10-11 03:36:23 +0300

    22-10-11

--- src/test/java/stepDefinitions/HerOkuAppStepDefinition.java
+++ src/test/java/stepDefinitions/HerOkuAppStepDefinition.java
@@ -3,41 +3,40 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.HerOkuAppPage;
-import utilities.Driver;
+import utilities.ReusableMethod;
 
-import java.time.Duration;
 
 public class HerOkuAppStepDefinition {
-    HerOkuAppPage herOkuAppPage=new HerOkuAppPage();
+    HerOkuAppPage page = new HerOkuAppPage();
 
     @Given("add element butonuna basar")
     public void add_element_butonuna_basar() {
-        herOkuAppPage.addButonu.click();
 
+        page.addButonu.click();
     }
+
     @Then("Delete butonu gorunur oluncaya kadar bekler")
     public void delete_butonu_gorunur_oluncaya_kadar_bekler() {
-        WebDriverWait wait=new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOf(herOkuAppPage.deleteButonu));
 
+        ReusableMethod.visibilityOfWait(page.deleteButonu);
     }
+
     @Then("Delete butonunun gorunur oldugunu test eder")
     public void delete_butonunun_gorunur_oldugunu_test_eder() {
-        Assert.assertTrue(herOkuAppPage.deleteButonu.isDisplayed());
 
+        Assert.assertTrue(page.deleteButonu.isDisplayed());
     }
+
     @Then("Delete butonuna basar")
     public void delete_butonuna_basar() {
-        herOkuAppPage.deleteButonu.click();
 
+        page.deleteButonu.click();
     }
+
     @Then("Delete butonunun gorunmedigini test eder")
     public void delete_butonunun_gorunmedigini_test_eder() {
 
-        Assert.assertTrue(herOkuAppPage.deleteListesi.isEmpty());
-
+        Assert.assertTrue(page.deleteListesi.isEmpty());
     }
 }
